30df00ec09ffe50bf4176c70b0aa27356cb67a27
RachinduAbeysekara <126750757+RachinduAbeysekara@users.noreply.github.com>
2024-05-12 10:44:36 +0530

    Delete Web directory

--- Web/db/db.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE lung_cancer;
--- Web/test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-# Load the trained model
-with open('./model/lung_cancer_prediction_model.pkl', 'rb') as file:
-    model = pickle.load(file)
-
-# Get the input shape of the model
-input_shape = model.input_shape
-print("Input shape:", input_shape)
